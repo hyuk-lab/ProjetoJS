a99c3ca8bef5108c6e58a7c65b9fec41a8e5aca7
hyuk-lab <goliveiramisael18@gmail.com>
2024-09-10 22:04:01 -0300

    Legal

---
+++ App.js
@@ -0,0 +1,198 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
+/*
+export default function App() {
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [operation, setOperation] = useState('add');
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleCalculate = async () => {
+    try {
+      setError(null); // Reset error
+      const response = await fetch(
+        `http://172.16.7.16:3000/calculate?num1=${num1}&num2=${num2}&operation=${operation}`
+      );
+      const data = await response.json();
+
+      if (response.ok) {
+        setResult(data.result);
+      } else {
+        setError(data.error);
+        setResult(null);
+      }
+    } catch (err) {
+      setError('Erro de rede ou servidor!');
+      setResult(null);
+    }
+  };
+
+  const getButtonStyle = (op) => {
+    return operation === op ? styles.selectedButton : styles.button;
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Calculadora</Text>
+
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        placeholder="Número 1"
+        value={num1}
+        onChangeText={setNum1}
+      />
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        placeholder="Número 2"
+        value={num2}
+        onChangeText={setNum2}
+      />
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={getButtonStyle('+')}
+          onPress={() => setOperation('+')}
+        >
+          <Text style={styles.buttonText}>Adicionar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={getButtonStyle('-')}
+          onPress={() => setOperation('-')}
+        >
+          <Text style={styles.buttonText}>Subtrair</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={getButtonStyle('x')}
+          onPress={() => setOperation('x')}
+        >
+          <Text style={styles.buttonText}>Multiplicar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={getButtonStyle('/')}
+          onPress={() => setOperation('/')}
+        >
+          <Text style={styles.buttonText}>Dividir</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Button title="Calcular" onPress={handleCalculate} />
+
+      {result !== null && <Text style={styles.result}>Resultado: {result}</Text>}
+      {error && <Text style={styles.error}>Erro: {error}</Text>}
+    </View>
+  );
+}
+*/
+export default function App() {
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+  const [result1, setResult1] = useState(null);
+  const [error, setError] = useState(null);
+
+
+  const handleCalculateImc = async () => {
+    try {
+      setError(null); // Reset error
+      const response = await fetch(
+        `http://172.16.7.16:3000/imc?peso=${peso}&altura=${altura}`
+      );
+      const data = await response.json();
+
+      if (response.ok) {
+        setResult1(data.result1);
+      } else {
+        setError(data.error);
+        setResult1(null);
+      }
+    } catch (err) {
+      setError('Erro de rede ou servidor!');
+      setResult(null);
+    }
+  };
+
+  /*const getButtonStyle = (op) => {
+    return operation === op ? styles.selectedButton : styles.button;
+  };*/
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>IMC</Text>
+
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        placeholder="Peso"
+        value={peso}
+        onChangeText={setPeso}
+      />
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        placeholder="Altura"
+        value={altura}
+        onChangeText={setAltura}
+      />
+
+      <Button title="Calcular" onPress={handleCalculateImc} />
+
+      {result1 !== null && <Text style={styles.result}>Resultado: {result1}</Text>}
+      {error && <Text style={styles.error}>Erro: {error}</Text>}
+    </View>
+  );
+}
+
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 16,
+  },
+  button: {
+    backgroundColor: 'lightgray',
+    padding: 10,
+    borderRadius: 5,
+  },
+  selectedButton: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  result: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginTop: 16,
+  },
+  error: {
+    fontSize: 18,
+    color: 'red',
+    textAlign: 'center',
+    marginTop: 16,
+  },
+});
